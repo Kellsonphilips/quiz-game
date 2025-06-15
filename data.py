9b2a7a9b0de75d11475d95909c4ea2a609201ee8
kellsonphilips <kellsonphilips@gmail.com>
2025-06-15 10:30:20 +0100

    score

---
+++ data.py
@@ -0,0 +1,43 @@
+question_data = [
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, "
+             "you are free to take it home to eat.", "answer": "True"},
+    {"text": "In London, UK, if you happen to die in the House of Parliament, "
+             "you are entitled to a state funeral.", "answer": "False"},
+    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"},
+    {"text": "Video streaming website YouTube was purchased in it&#039;s entirety by "
+             "Facebook for US$1.65 billion in stock.", "answer": "False"},
+    {"text": "Gumbo is a stew that originated in Louisiana.", "answer": "True"},
+    {"text": "It is automatically considered entrapment in the United States "
+             "if the police sell you illegal substances without revealing themselves.", "answer": "False"},
+    {"text": "Nutella is produced by the German company Ferrero.", "answer": "False"},
+    {"text": "Bulls are attracted to the color red.", "answer": "False"},
+    {"text": "The Sun rises from the North.", "answer": "False"},
+    {"text": "You can legally drink alcohol while driving in Mississippi.", "answer": "True"},
+    {"text": "On average, at least 1 person is killed by a drunk driver in the United States every hour.",
+     "answer": "True"},
+    {"text": "In 2010, Twitter and the United States Library of Congress partnered together "
+             "to archive every tweet by American citizens.", "answer": "True"},
+    {"text": "Pluto is a planet.", "answer": "False"},
+    {"text": "Adolf Hitler was born in Australia. ", "answer": "False"},
+    {"text": "When you cry in space, your tears stick to your face.", "answer": "True"},
+    {"text": "Romanian belongs to the Romance language family, shared with French,"
+             " Spanish, Portuguese and Italian. ", "answer": "True"},
+    {"text": "Dihydrogen Monoxide was banned due to health risks after being discovered in 1983 "
+             "inside swimming pools and drinking water.", "answer": "False"},
+    {"text": "Scotland voted to become an independent country during the referendum from September 2014.",
+     "answer": "False"},
+    {"text": "The National Animal of Scotland is the Unicorn.", "answer": "True"},
+    {"text": "Vietnam&#039;s national flag is a red star in front of a yellow background.", "answer": "False"},
+    {"text": "A pasodoble is a type of Italian pasta sauce.", "answer": "False"},
+    {"text": "Slovakia is a member of European Union-", "answer": "True"},
+    {"text": "Ping-Pong originated in England", "answer": "True"}
+]
